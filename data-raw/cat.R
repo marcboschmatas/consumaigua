cda569b9db67f52d5a7c1ea20e65e0e64b774e51
marcboschmatas <mboschmatas@gmail.com>
2023-06-27 23:06:11 +0200

    reworking stuff

--- data-raw/cat.R
+++ data-raw/cat.R
@@ -1,7 +1,7 @@
 # script to prepare `cat` dataset
 
 
-cat <- sf::read_sf("~/final_postgrau/data/cat_borders.geojson")
+#cat <- sf::read_sf("~/final_postgrau/data/cat_borders.geojson")
 
 usethis::use_data(cat, overwrite = TRUE)
 
